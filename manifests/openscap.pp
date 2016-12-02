d82faf7b547f17a2c592bef37797a5e138d52a98
Hal Deadman <deadman_harold@bah.com>
2016-12-02 09:28:02 -0500

    configure openscap repo to be skipped if  unavailable

--- manifests/openscap.pp
+++ manifests/openscap.pp
@@ -3,13 +3,14 @@
 # Installs the Openscap yum repository.
 #
 class yumrepos::openscap (
-  $openscap_url = $yumrepos::params::openscap_url,
-  $openscap_enabled = $yumrepos::params::openscap_enabled,
-  $openscap_gpgcheck = $yumrepos::params::openscap_gpgcheck,
-  $openscap_includepkgs = $yumrepos::params::openscap_includepkgs,
-  $openscap_exclude = $yumrepos::params::openscap_exclude,
-) inherits yumrepos::params {
-
+  $openscap_url                 = $yumrepos::params::openscap_url,
+  $openscap_enabled             = $yumrepos::params::openscap_enabled,
+  $openscap_gpgcheck            = $yumrepos::params::openscap_gpgcheck,
+  $openscap_includepkgs         = $yumrepos::params::openscap_includepkgs,
+  $openscap_exclude             = $yumrepos::params::openscap_exclude,
+  $openscap_skip_if_unavailable = true,
+  ) inherits yumrepos::params {
+  
   file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-openscap":
     ensure => present,
     owner  => root,
@@ -19,18 +20,17 @@ class yumrepos::openscap (
   }
 
   yumrepo { 'openscap-latest':
-    descr       => 'Openscap Latest',
-    baseurl     => $openscap_url,
-    enabled     => $openscap_enabled,
-    gpgcheck    => $openscap_gpgcheck,
-    includepkgs => $openscap_includepkgs,
-    exclude     => $openscap_exclude,
-    gpgkey      => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-openscap",
-    require     => File["/etc/pki/rpm-gpg/RPM-GPG-KEY-openscap"],
+    descr               => 'Openscap Latest',
+    baseurl             => $openscap_url,
+    enabled             => $openscap_enabled,
+    gpgcheck            => $openscap_gpgcheck,
+    includepkgs         => $openscap_includepkgs,
+    exclude             => $openscap_exclude,
+    skip_if_unavailable => $openscap_skip_if_unavailable,
+    gpgkey              => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-openscap",
+    require             => File["/etc/pki/rpm-gpg/RPM-GPG-KEY-openscap"],
   }
 
-  yumrepos::rpm_gpg_key { "openscap-latest":
-      path => "/etc/pki/rpm-gpg/RPM-GPG-KEY-openscap",
-  }
+  yumrepos::rpm_gpg_key { "openscap-latest": path => "/etc/pki/rpm-gpg/RPM-GPG-KEY-openscap", }
 
 }
